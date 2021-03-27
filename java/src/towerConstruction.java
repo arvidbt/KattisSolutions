38afe346171af23d73145ea2c6e77f7ba19e9514
arvidbt <arvidbergmanthorn@gmail.com>
2021-03-27 17:39:42 +0100

    finished

--- java/src/towerConstruction.java
+++ java/src/towerConstruction.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class towerConstruction {
+import java.util.Scanner;
+
+public class towerConstruction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int towers = 0;
+        int brick = 0;
+
+        for(int i = 0; i < n; i++){
+            int currentBrick = brick;
+            brick = scanner.nextInt();
+            if(currentBrick < brick){
+                towers++;
+            }
+        }
+        System.out.println(towers);
+    }
 }
