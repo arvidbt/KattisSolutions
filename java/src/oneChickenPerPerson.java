1105576ecf5cde881a42d96dc14910fa273b57cc
arvidbt <arvidbergmanthorn@gmail.com>
2021-03-29 13:38:28 +0200

    :)

--- java/src/oneChickenPerPerson.java
+++ java/src/oneChickenPerPerson.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class oneChickenPerPerson {
+import java.util.Scanner;
+
+public class oneChickenPerPerson {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int guests = scanner.nextInt();
+        int chickens = scanner.nextInt();
+
+        if(chickens > guests){
+            int ate = chickens - guests;
+
+            if(ate == 1){
+                System.out.println("Dr. Chaz will have " + ate + " piece of chicken left over!");
+
+            }
+            else {
+                System.out.println("Dr. Chaz will have " + ate +" pieces of chicken left over!");
+            }
+        }
+        else{
+            int left = guests - chickens;
+
+            if(left == 1){
+                System.out.println("Dr. Chaz needs " + left + " more piece of chicken!");
+            }
+            else{
+                System.out.println("Dr. Chaz needs " + left + " more pieces of chicken!");
+
+            }
+        }
+    }
 }
